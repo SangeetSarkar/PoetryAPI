be9adabf461c51a3d4249229ce6ffd6c802e5a8d
Sangeet Sarkar <sangeetsarkar2002@gmail.com>
2023-09-19 10:31:23 +0530

    Added quotes (1- 13)

--- data.js
+++ data.js
@@ -1,15 +1,43 @@
 const poemData = [
     {
-        author: "James Baldwin",
-        poem_text: "You think your pain and your heartbreak are unprecedented in the history of the world, but then you read"
+        author: "Yukio Mishima",
+        poem_text: "Young people get the foolish idea that what is new for them must be new for everybody else too. No matter how unconventional they get, they're just repeating what others before them have done"
     },
     {
-        author: "Oscar Wilde",
-        poem_text: "Be yourself; everyone else is already taken"
+        author: "Yasunari Kawabata",
+        poem_text: "People have separated from each other with walls of concrete that blocked the roads to connection and love. and Nature has been defeated in the name of development"
     },
     {
-        author: "Mark Twain",
-        poem_text: "If you tell the truth, you don't have to remember anything"
+        author: "Natsume Soseki",
+        poem_text: "Words are not meant to stir the air only: they are capable of moving greater things"
+    },
+    {
+        author: 'Eiji Yoshikawa',
+        poem_text: 'Not only must a warrior be strong with his bow, but he must have a heart full of pity for all living creatures'
+    },
+    {
+        author:"Fyodor Dostoevsky",
+        poem_text:"Man only likes to count his troubles; he doesn't calculate his happiness"
+    },
+    {
+        author:'Steve Jobs',
+        poem_text: "People who are crazy enough to think they can change the world, are the ones who do"
+    },
+    {
+        author: 'Maya Angelou',
+        poem_text: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel"
+    },
+    {
+        author: 'Elbert Hubbard',
+        poem_text: "A friend is someone who knows all about you and still loves you"
+    },
+    {
+        author:'William Shakespeare',
+        poem_text:"The fool doth think he is wise, but the wise man knows himself to be a fool"
+    },
+    {
+        author: 'Pablo Neruda',
+        poem_text: "I love you without knowing how, or when, or from where. I love you simply, without problems or pride: I love you in this way because I do not know any other way of loving but this, in which there is no I or you, so intimate that your hand upon my chest is my hand, so intimate that when I fall asleep your eyes close."
     }
 ]
 
